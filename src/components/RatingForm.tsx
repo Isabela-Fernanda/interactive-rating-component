8dfb924a71ad2fb3912433dfc31328110919e53a
Isabela Fernanda <arty.7f@gmail.com>
2026-08-18 22:01:59 -0300

    Applying thank desktop style

--- src/components/RatingForm.tsx
+++ src/components/RatingForm.tsx
@@ -10,18 +10,18 @@ const ratings = [1, 2, 3, 4, 5];
 
 export function RatingForm({ rating, onRatingChange, onSubmit, }: RatingFormProps) {
     return (
-        <form onSubmit={onSubmit} className="bg-grey-900 w-82 h-90 rounded-xl px-6 py-5.5 lg:w-103 lg:h-104 lg:rounded-4xl lg:px-8 lg:py-8">
+        <form onSubmit={onSubmit} className="bg-grey-900 w-82 h-90 rounded-xl px-6 py-5.5 lg:w-103 lg:h-104 lg:rounded-4xl lg:pl-8 lg:pr-10 lg:py-8">
             <div className="bg-grey-500/20 rounded-full w-10 h-10 flex items-center justify-center mb-5.5 lg:w-12 lg:h-12 lg:mb-9">
                 <img src={StarIcon} alt="" className="h-3 lg:h-4" />
             </div>
 
 
             <fieldset aria-describedby="rating-description">
-                <legend className="text-2xl font-bold mb-3 lg:text-3xl lg:tracking-tighter lg:mb-2.5">How did we do?</legend>
+                <legend className="text-2xl font-bold mb-3 lg:text-3xl lg:tracking-tighter lg:mb-2">How did we do?</legend>
 
-                <p id="rating-description" className="tracking-tight lg:tracking-normal">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering! </p>
+                <p id="rating-description" className="text-grey-500 tracking-tight lg:tracking-normal lg:leading-6">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering! </p>
 
-                <div className="flex justify-between my-5 lg:my-6.5">
+                <div className="flex justify-between my-5 lg:my-6">
                     {ratings.map((value) => (
                         <label key={value} className="cursor-pointer" >
                             <input type="radio" name="rating" value={value} checked={rating === value} onChange={() => onRatingChange(value)} required className="peer sr-only"/>
@@ -34,7 +34,7 @@ export function RatingForm({ rating, onRatingChange, onSubmit, }: RatingFormProp
                 </div>
             </fieldset>
 
-            <button type="submit" className="bg-orange-500 mt-1 w-full h-11.5 rounded-r-full rounded-l-full font-bold tracking-widest text-grey-950 hover:bg-white-0">SUBMIT</button>
+            <button type="submit" className="bg-orange-500 mt-1 w-full h-11.5 rounded-r-full rounded-l-full font-bold tracking-widest text-grey-950 hover:bg-white-0 lg:mt-1.5">SUBMIT</button>
         </form>
     );
 }
--- src/components/ThankYou.tsx
+++ src/components/ThankYou.tsx
@@ -14,14 +14,14 @@ export function ThankYou({ rating }: ThankYouProps) {
     }, []);
 
     return (
-        <section className="bg-grey-900 w-82 h-90 lg:w-103 rounded-xl px-6 py-5.5 flex flex-col items-center">
-            <img src={Illustration} alt="" className="h-24 w-36 mt-3 mb-6" />
+        <section className="bg-grey-900 w-82 h-90 rounded-xl px-6 py-5.5 flex flex-col items-center lg:w-103 lg:h-104 lg:rounded-4xl lg:px-7 lg:py-8">
+            <img src={Illustration} alt="" className="h-24 w-36 mt-3 mb-6 lg:h-27 lg:w-41 lg:mb-8" />
 
-            <p className="bg-grey-500/20 py-1 px-2.5 rounded-2xl tracking-tight text-orange-500">You selected {rating} out of 5</p>
+            <p className="bg-grey-500/20 py-1 px-2.5 rounded-2xl tracking-tight text-orange-500 lg:py-1.5 lg:px-6">You selected {rating} out of 5</p>
 
-            <h1 ref={headingRef} tabIndex={-1} className="mt-7.5 mb-2.5 text-2xl">Thank you!</h1>
+            <h1 ref={headingRef} tabIndex={-1} className="mt-7.5 mb-2.5 text-2xl lg:mt-9.5 lg:text-3xl lg:mb-2 lg:font-bold">Thank you!</h1>
 
-            <p className="text-center tracking-tight">We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!</p>
+            <p className="text-grey-500 text-center tracking-tight lg:tracking-wide lg:leading-6">We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!</p>
         </section>
     );
 }
